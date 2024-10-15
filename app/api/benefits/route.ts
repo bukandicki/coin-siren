cd037b3b6d7e2eb045922731d2ba43e9559140dc
Dicki Maulana <bukandicki@gmail.com>
2024-10-15 16:22:56 +0700

    feat(api): init benefits api

---
+++ app/api/benefits/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+  try {
+    const filePath = path.join(process.cwd(), 'lib/json', 'benefits.json');
+    const fileContents = await fs.promises.readFile(filePath, 'utf8');
+
+    const data = JSON.parse(fileContents);
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error('Error reading file:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
