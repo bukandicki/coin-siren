73277630318be9114d83b715d048a3349872ff95
Dicki Maulana <bukandicki@gmail.com>
2024-10-15 21:57:11 +0700

    style: init header styling

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,48 @@
+import { MENUS } from '@/lib/constants';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
 import './Header.styles.css';
 
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="Header">
+      <Image className="Header__logo" src="/svgs/logo.svg" width={144} height={21} alt="Hyperhire Logo" />
+
+      <nav className="Header__nav">
+        <ul className="Nav__lists">
+          {MENUS.map((menu) => (
+            <li className="Nav__item" key={menu.title}>
+              {menu.sub_items.length ? (
+                <div className="Nav__dropdown">
+                  <div className="Dropdown__title">
+                    <span>{menu.title}</span>
+
+                    <Image src="/svgs/icon-caret-down.svg" width={16} height={16} alt="Caret" />
+                  </div>
+
+                  <ul className="Dropdown">
+                    {menu.sub_items.map((sub) => (
+                      <li className="Dropdown__item" key={sub.title}>
+                        <Link className="Dropdown__link" href={sub.href}>
+                          {sub.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ) : (
+                <Link className="Nav__link" href={menu.href}>
+                  {menu.title}
+                </Link>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <button className="Header__cta">문의하기</button>
+    </header>
+  );
 }
