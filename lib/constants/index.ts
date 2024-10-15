d62d53bb93996e32fc5533fa727988ea7b24f060
Dicki Maulana <bukandicki@gmail.com>
2024-10-15 21:56:01 +0700

    add menu constant

---
+++ lib/constants/index.ts
@@ -0,0 +1,29 @@
+export const MENUS = [
+  {
+    title: '채용',
+    href: '',
+    sub_items: [
+      {
+        title: '채용',
+        href: '/',
+      },
+      {
+        title: '해외 개발자 원격 채용',
+        href: '/',
+      },
+      {
+        title: '외국인 원격 채용 (비개발 직군)',
+        href: '/',
+      },
+      {
+        title: '한국어 가능 외국인 채용',
+        href: '/',
+      },
+    ],
+  },
+  {
+    title: '해외 개발자 활용 서비스',
+    href: '/',
+    sub_items: [],
+  },
+];
