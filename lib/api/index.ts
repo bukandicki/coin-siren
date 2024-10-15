be1600ec002dea3c0ac90144b970fe143c5df5cf
Dicki Maulana <bukandicki@gmail.com>
2024-10-15 17:44:16 +0700

    update api utils function return type

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,20 +1,20 @@
-import { API_RESPONSE, BENEFIT_TYPE, HIGHLIGHT_TYPE, USER_TYPE } from '../types';
+import { BENEFIT_TYPE, HIGHLIGHT_TYPE, USER_TYPE } from '../types';
 
-export async function getUsers(): Promise<API_RESPONSE<USER_TYPE>> {
+export async function getUsers(): Promise<USER_TYPE[]> {
   const res = await fetch('http://localhost:3000/api/users');
   if (!res.ok) throw new Error('Failed to fetch users data');
 
   return res.json();
 }
 
-export async function getHighlights(): Promise<API_RESPONSE<HIGHLIGHT_TYPE>> {
+export async function getHighlights(): Promise<HIGHLIGHT_TYPE[]> {
   const res = await fetch('http://localhost:3000/api/highlights');
   if (!res.ok) throw new Error('Failed to fetch highlights data');
 
   return res.json();
 }
 
-export async function getBenefits(): Promise<API_RESPONSE<BENEFIT_TYPE>> {
+export async function getBenefits(): Promise<BENEFIT_TYPE[]> {
   const res = await fetch('http://localhost:3000/api/benefits');
   if (!res.ok) throw new Error('Failed to fetch benefits data');
 
