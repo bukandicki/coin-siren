c0ef53dc0cec3f8bb3704a2e66e4cd0a6453a4b9
Dicki Maulana <bukandicki@gmail.com>
2024-10-16 15:38:58 +0700

    add footer constant

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -27,3 +27,26 @@ export const MENUS = [
     sub_items: [],
   },
 ];
+
+export const FOOTER_BENEFITS = [
+  {
+    icon: '/svgs/icon-code.svg',
+    title: '해외 개발자 원격 채용',
+    href: '/',
+  },
+  {
+    icon: '/svgs/icon-avatar.svg',
+    title: '외국인 원격 채용 (비개발)',
+    href: '/',
+  },
+  {
+    icon: '/svgs/icon-kor.svg',
+    title: '한국어 가능 외국인 채용',
+    href: '/',
+  },
+  {
+    icon: '/svgs/icon-gear.svg',
+    title: '해외 개발자 활용 서비스',
+    href: '/',
+  },
+];
